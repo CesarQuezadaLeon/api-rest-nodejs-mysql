40143269445ab2a4484143b014af8447a8d8c52e
Cesar Raul Quezada Leon <cesar.raul.ql@gmail.com>
2023-02-14 13:22:33 -0600

    Practica terminada

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,10 @@
 import { pool } from '../db.js';
 
 export const pingController = async(req,res)=> {
-    const [result] = await pool.query('select 1+1 as result')
-    res.json(result)
+    try {
+        const [result] = await pool.query('select 1+1 as result')
+    res.json(result)    
+    } catch (error) {
+        console.log(error);
+    }   
 }
