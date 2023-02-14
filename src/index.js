8347ac76e21430c980526ac070f98d27d9cdae61
Cesar Raul Quezada Leon <cesar.raul.ql@gmail.com>
2023-02-14 13:21:14 -0600

    termino de practica

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-import express from "express";
-import {pool} from "./db.js"
-import employeeRoutes from './routes/employee.routes.js';
-import indexRoutes from './routes/index.routes.js';
+import {PORT} from './config.js'
+import app  from './app.js'
 
-const app = express();
-
-app.listen(3000);
-app.use(express.json())
-app.use(indexRoutes);
-app.use('/api/',employeeRoutes);
-
-console.log("Server is running in port 3000")
+app.listen(PORT);
+console.log("Server is running in port:",PORT)
